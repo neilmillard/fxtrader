9988b3401b385caa5a246b7381c6f9a98d6573c3
Neil Millard <neil.millard@gmail.com>
2015-11-04 17:33:58 +0000

    tidy output

--- public/github937409750924372dev.php
+++ public/github937409750924372dev.php
@@ -44,7 +44,7 @@ if ($tmp == 'neilmillard') {
 <pre>
  .  ____  .    ____________________________
  |/      \|   |                            |
-[|  <span style="color: #FF0000;">&hearts; . &hearts;</span> |]  | Git Deployment Script v0.1 |
+[|  <span style="color: #FF0000;">&hearts; &hearts;</span> |]  | Git Deployment Script v0.1 |
  |___==___|  / &copy; <a href="https://gist.github.com/oodavid">oodavid</a> 2012 |
               |____________________________|
 
