450051d7ffef239b30ea24163645ea07927aa042
Neil Millard <neil.millard@gmail.com>
2015-11-04 18:06:14 +0000

    log errors

--- public/github937409750924372dev.php
+++ public/github937409750924372dev.php
@@ -29,7 +29,7 @@ if ($tmp == 'neilmillard' && $data['ref'] == '/refs/heads/dev') {
 //        $output .= htmlentities(trim($tmp)) . "\n";
 //    }
 } else {
-    $output = ":$tmp: Wrong host";
+    \error_log("post hook error", 0);
 }
 
 // Make it pretty for manual user access (and why not?)
