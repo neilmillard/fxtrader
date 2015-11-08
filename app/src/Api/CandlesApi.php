7d8c05aae21b005290ff36d799bb16485bb91308
Neil Millard <neil.millard@gmail.com>
2015-11-08 18:40:31 +0000

    update graph on homescreen

--- app/src/Api/CandlesApi.php
+++ app/src/Api/CandlesApi.php
@@ -4,36 +4,80 @@ namespace App\Api;
 
 
 use App\Action\Controller;
+use GuzzleHttp\Psr7\LazyOpenStream;
+use RedBeanPHP\R;
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Slim\Http\Stream;
 
 class CandlesApi extends Controller
 {
+    /**
+     * @param Request $request | $args
+     * @param Response $response
+     * @param array $args
+     * @return Response containing candle data as a CSV file
+     */
     function dispatch(Request $request, Response $response, Array $args)
     {
         $id = $this->authenticator->getIdentity();
         if (empty($id)) {
             //TODO: return json instead
             $path = __DIR__;
-            $newStream = new \GuzzleHttp\Psr7\LazyOpenStream($path . '/../../../public/data.csv', 'r');
-            $newResponse = $response->withBody($newStream);
-            return $newResponse;
+            $newStream = new LazyOpenStream($path . '/../../../public/data.csv', 'r');
         } else {
             // get the current instrument and send some data
             //Defaults
-            $instrument =
-            //TODO time needs to be 17-aug-2015,
-            $data = json_encode([
-                'Date' => time(),
-                'Open' => 0.0034,
-                'High' => 0.2039,
-                'Low' => 0.2039,
-                'Close' => 0.2309,
-                'Volume' => 299344,
-            ]);
-        }
+            require_once __DIR__ . '/../../loadsettings.php';
+            $settings = loadsettings();
+            $pairs = $settings['oanda']['pairs'];
+            $grans = ['H1', 'D'];
+            $days = 200;
+            $fp = fopen('php://temp', 'r+b');
+            $newStream = new Stream($fp);
+            $newStream->write('Date,Open,High,Low,Close,Volume' . PHP_EOL);
+            $instrument = $request->getParam('instrument');
+            if (!in_array($instrument, $pairs)) {
+                $instrument = $pairs[1];
+            }
+            $gran = $request->getParam('gran'); // D or H1
+            if (!in_array($gran, $grans)) {
+                $gran = 'D';
+            }
+            $candles = R::find(
+                'candle',
+                ' instrument = :instrument AND gran = :gran ORDER BY date DESC LIMIT :days',
+                [
+                    ':instrument' => $instrument,
+                    ':gran' => $gran,
+                    ':days' => $days
+                ]
+            );
+            if (!empty($candles)) {
+                foreach ($candles as $candle) {
+                    //TODO time needs to be 17-aug-2015,
+                    $data = [];
+                    $candletime = new \DateTime('@' . $candle->candletime);
+                    if ($gran == 'D') {
+                        $data['Date'] = $candletime->format('d-M-y');
+                    } else {
+                        $data['Date'] = $candletime->format('Y-m-d H:i:s');
+                    }
+                    $data['Open'] = $candle->open;
+                    $data['High'] = $candle->high;
+                    $data['Low'] = $candle->low;
+                    $data['Close'] = $candle->close;
+                    $data['Volume'] = '0';
+                    fputcsv($fp, $data, $delimiter = ',', $enclosure = '"');
 
-        $response->withJson($data);
-        return $response;
+                }
+            }
+
+
+            $newStream->rewind();
+
+        }
+        $newResponse = $response->withBody($newStream);
+        return $newResponse;
     }
 }
---
+++ public/scripts/mainchart.js
@@ -0,0 +1,85 @@
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var parseDate = d3.time.format("%d-%b-%y").parse;
+
+var x = techan.scale.financetime()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var candlestick = techan.plot.candlestick()
+    .xScale(x)
+    .yScale(y);
+
+var atrtrailingstop = techan.plot.atrtrailingstop()
+    .xScale(x)
+    .yScale(y);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .tickFormat(d3.format(",.4f"));
+
+var svg = d3.select("div.chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+svg.append("clipPath")
+    .attr("id", "clip")
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", y(1))
+    .attr("width", width)
+    .attr("height", y(0) - y(1));
+
+d3.csv("/api/candles", function (error, data) {
+    var accessor = candlestick.accessor();
+
+    data = data.map(function (d) {
+        // Open, high, low, close generally not required, is being used here to demonstrate colored volume
+        // bars
+        return {
+            date: parseDate(d.Date),
+            volume: +d.Volume,
+            open: +d.Open,
+            high: +d.High,
+            low: +d.Low,
+            close: +d.Close
+        };
+    }).sort(function (a, b) {
+        return d3.ascending(accessor.d(a), accessor.d(b));
+    });
+
+    var atrtrailingstopData = techan.indicator.atrtrailingstop()(data);
+    x.domain(data.map(accessor.d));
+    y.domain(techan.scale.plot.atrtrailingstop(atrtrailingstopData).domain());
+
+    svg.append("g")
+        .datum(data)
+        .attr("class", "candlestick")
+        .attr("clip-path", "url(#clip)")
+        .call(candlestick);
+
+    svg.append("g")
+        .datum(atrtrailingstopData)
+        .attr("class", "atrtrailingstop")
+        .call(atrtrailingstop);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+});
