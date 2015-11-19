33c0713c0aec64344d2965672d3d04c6d0b9d05b
neilmillard <neil.millard@gmail.com>
2015-11-19 22:09:30 +0000

    try expiry check

--- app/src/Job/Analyse.php
+++ app/src/Job/Analyse.php
@@ -62,18 +62,7 @@ class Analyse extends Job
         /* @var \App\Signal $analysisClass */
         $analysisClass = new $signalClass($this->args['params'],$candles);
         $result = $analysisClass->analyse();
-        if($endtime == 1445806800){
-            $this->logger->log(
-                \Psr\Log\LogLevel::INFO,
-                'Processed {instrument} candle @{endTime}| Result was {result}',
-                array('instrument' => $this->args['instrument'],
-                    'endTime'      => $endtime,
-                    'result'       => print_r($result,true)
-                    
-                )
-            );
-        }
-        if($result['trade']){
+        if($result['expiry']>0){
             $recommendation = new Model_Recommendations($result);
             $recommendation->setStrategy( $this->args['strategyId'] );
             $this->logger->log(
